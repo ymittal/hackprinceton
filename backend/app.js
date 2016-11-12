95f99d7dc2a685b91eecc90d499816f4cb0f495a
Kinzeng <zhang4444@gmail.com>
2016-11-12 16:00:09 -0500

    convert backend to es6, create reset-db

--- backend/app.js
+++ backend/app.js
@@ -1,47 +1,43 @@
-require('./db')
+import './db'
 
-var express = require('express')
-var bodyParser = require('body-parser')
-var mongoose = require('mongoose')
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
 
 var app = express()
 app.use(bodyParser.urlencoded({extended: false}))
-app.use(function (req, res, next) {
-	req.hello = 'world'
-	console.log(req.method + ' ' + req.path)
-	next()
+
+const User = mongoose.model('User')
+const Group = mongoose.model('Group')
+const Donation = mongoose.model('Donation')
+
+app.get('/users', (req, res, next) => {
+  User.findOne({username: req.query.username}).populate('group', function (err, data, count) {
+    if (err) next(err)
+    else res.send(data)
+  })
 })
 
-app.get('/users', function (req, res, next) {
-	console.log(req.hello)
-	User.findOne({username: req.query.username}).populate('group', function (err, data, count) {
-		if (err) next(err);
-		else res.send(data)
-	})
+app.get('/groups', (req, res) => {})
+
+app.post('/donations', (req, res) => {
+  var donation = new Donation({
+    amount: req.body.amount,
+    user: req.body.userName,
+    group: req.body.theme
+  })
+
+  donation.save((err) => {
+    if (err) throw err
+
+    console.log('Donation created!')
+  })
 })
 
-app.get('/groups', function (req,res){
-
+app.use((err, req, res, next) => {
+  console.log('error', err)
 })
 
-app.post('/donations', function (req,res){
-	var donation = new Donation({
-		amount: req.body.amount,
-		user: req.body.userName,
-		group: req.body.theme
-	})
-
-	donatione.save(function (err) {
-		if (err) throw err;
-
-		console.log('Donation created!')
-	})
-});
-
-app.use(function (err, req, res, next) {
-	console.log('error', err)
-})
-
-app.listen(8080, 'localhost', function () {
-	console.log('App listening on http://localhost:8080')
+app.listen(8080, 'localhost', () => {
+  console.log('App listening on http://localhost:8080')
 })
--- backend/db.js
+++ backend/db.js
@@ -1,23 +1,21 @@
-//connecting mongoose
+// connecting mongoose
 
-var mongoose = require('mongoose');
+import mongoose, {Schema} from 'mongoose'
 
-mongoose.connect('mongodb://piyushib:mongodb@ec2-54-196-207-87.compute-1.amazonaws.com:27017/admin');
+mongoose.connect('mongodb://piyushib:mongodb@ec2-54-196-207-87.compute-1.amazonaws.com:27017/admin')
 
-//get notified if we connected successfully
+// get notified if we connected successfully
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  // we're connected!
-});
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'Connection error:'))
+db.once('open', console.log.bind(console, 'Connection established'))
 
-//reference to user schema
-var userSchema = mongoose.Schema({
-	username: {
-		type: String,
-		required: true
-	},
+// reference to user schema
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true
+  },
   firstName: {
     type: String,
     required: true
@@ -26,50 +24,50 @@ var userSchema = mongoose.Schema({
     type: String,
     required: true
   },
-	groups: [{
-		type: mongoose.Schema.types.ObjectId,
-		ref: 'Group'
-	}]
+  groups: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Group'
+  }]
 })
 
-//compiled schema into a model
-var User = mongoose.model('User', userSchema);
+// compiled schema into a model
+mongoose.model('User', userSchema)
 
 // group schema
-var groupSchema = new mongoose.Schema({
+const groupSchema = new Schema({
   users: [{type: Schema.Types.ObjectId, ref: 'userSchema'}],
   timeLeft: Number,
   goal: {
     type: Number,
     required: true
-  }
+  },
   theme: {
     type: String,
     required: true
-  }
+  },
   mission: String
-});
+})
 
-var Group = mongoose.model('Group', groupSchema);
+mongoose.model('Group', groupSchema)
 
 // donation schema
-var donationSchema = new mongoose.Schema({
+const donationSchema = new Schema({
   amount: {
     type: Number,
     required: true
-  }
+  },
   groupAssociated: [{
     type: Schema.Types.ObjectId, ref: 'group'
   }],
-  userAssociated:[{
+  userAssociated: [{
     type: Schema.Types.ObjectId, ref: 'user'
   }]
-});
+})
 
-var Donation = mongoose.model('Donation', donationSchema);
+mongoose.model('Donation', donationSchema)
 
-//payments schema
-var paymentSchema = new mongoose.Schema({
+// payments schema
+const paymentSchema = new Schema({
   amount: {
     type: Number,
     required: true
@@ -80,6 +78,6 @@ var paymentSchema = new mongoose.Schema({
   groupAssociated: {
     type: Schema.Types.ObjectId, ref: 'Group'
   }
-});
+})
 
-var Payment = mongoose.model('Payment', paymentSchema);
+mongoose.model('Payment', paymentSchema)
